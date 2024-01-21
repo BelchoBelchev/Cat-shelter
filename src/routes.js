334de1d1ecd1efb086739e85dc96d9c13bfb7f64
Belcho Belchev <whitybelchev@gmail.com>
2024-01-21 18:38:15 +0200

    Setup add breed section

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const addCatController = require('./controllers/addCatCotroller');
+const addBreedContoller = require('./controllers/addBreedController');
 
 router.use(homeController);
 router.use(addCatController);
+router.use(addBreedContoller);
 
 module.exports = router;
